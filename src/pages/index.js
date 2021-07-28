99fa6b7dd8716acc363b8c44e8bf03c424f5e9d2
Kamil Pawlak <ricoet22@gmail.com>
2021-07-28 19:50:39 +0200

    feat: homepage technical help section

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,7 @@ import Blog from "../sections/home1/Blog";
 import OurExperience from "../sections/home1/OurExperience";
 import Stats from "../sections/home1/Stats";
 import Events from "../sections/home1/Events";
+import TechHelp from "../sections/home1/TechHelp";
 
 const IndexPage = () => {
   return (
@@ -32,6 +33,7 @@ const IndexPage = () => {
         <OurExperience className="py-12 py-lg-25" />
         <Stats />
         <Events />
+        <TechHelp className="bg-default-1 pt-12 pt-lg-25 pb-8 pb-lg-20" />
         <Company className="bg-default-1 pt-13 pt-md-17 pt-lg-24 pb-13 pb-md-14 pb-lg-23" />
         <Categories className="bg-default-1 pt-lg-13 pb-md-7 pb-lg-11 position-relative" />
         <Jobs className="pt-14 pt-md-18 pt-lg-30 bg-default-2 pb-9 pb-md-13 pb-lg-29" />
---
+++ src/sections/home1/TechHelp.js
@@ -0,0 +1,74 @@
+import React from "react";
+
+import img1 from "../../assets/events_background.png";
+import img2 from "../../assets/devnetic.png";
+import { Card } from "react-bootstrap";
+
+const TechHelp = ({ className, ...rest }) => {
+  return (
+    <>
+      <div className={className} {...rest}>
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12">
+              <div
+                className="section-title"
+                data-aos="fade-left"
+                data-aos-delay={500}
+                data-aos-once="true" data-aos-duration={1000}
+              >
+                <h2 className="font-size-11 mb-12">
+                  Technical help
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div className="row my-n5">
+            <div className="col-lg-6 my-5">
+              <Card
+                className="bg-white shadow-2 border-0 h-100"
+                data-aos="fade-right"
+                data-aos-delay={500}
+                data-aos-once="true" data-aos-duration={1000}
+              >
+                <Card.Title className="my-7 px-7">Mentorship Program</Card.Title>
+                <hr className="mx-12 my-2" />
+                <Card.Body className="d-flex flex-column justify-content-between">
+                  <p>
+                    App Dev League’s mentorship program is a tutoring program where students can learn Computer Science 1-on-1.
+                  </p>
+                  <ul>
+                    <li className="text-default-color"><strong>Flexible -</strong> Litany of topics and adjustable course agenda</li>
+                    <li><strong>Interactive -</strong> Engaging lesson slides, kahoots, and fun quizzes</li>
+                    <li><strong>Hands On -</strong> Numerous projects along the course</li>
+                    <li><strong>Personalized Help -</strong> 1 v 1 sessions with personalized content</li>
+                  </ul>
+                  <img src={img1} alt="" className="d-block w-75 mx-auto mt-8 rounded-lg"/>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="col-lg-6 my-5">
+              <Card
+                className="bg-white shadow-2 border-0 h-100"
+                data-aos="fade-left"
+                data-aos-delay={500}
+                data-aos-once="true" data-aos-duration={1000}
+              >
+                <Card.Title className="my-7 px-7">Devnetic App</Card.Title>
+                <hr className="mx-12 my-2" />
+                <Card.Body className="d-flex flex-column justify-content-between">
+                  <p>
+                    App Dev League is launching an interactive teaching app for students to learn about Application Development and AI through a series of interactive multiple choice questions, videos, as well as a gamified leveling up system.
+                  </p>
+                  <img src={img2} alt="" className="d-block w-75 mx-auto  rounded-lg"/>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TechHelp;
