c3ef6c7252f807926afe1d5e6b9a7da70e1b804b
Kamil Pawlak <ricoet22@gmail.com>
2021-07-28 21:08:28 +0200

    chore: remove unused components in index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,6 @@ import Head from "next/head";
 import PageWrapper from "../components/PageWrapper";
 import Hero from "../sections/home1/Hero";
 import AboutUs from "../sections/home1/AboutUs";
-import Company from "../sections/home1/Compnay";
-import Categories from "../sections/home1/Categories";
-import Jobs from "../sections/home1/Jobs";
-import Content1 from "../sections/home1/Content1";
-import Blog from "../sections/home1/Blog";
-// import Section from "../layout/Section";
 import OurExperience from "../sections/home1/OurExperience";
 import Stats from "../sections/home1/Stats";
 import Events from "../sections/home1/Events";
@@ -38,11 +32,6 @@ const IndexPage = () => {
         <TechHelp className="bg-default-1 pt-12 pt-lg-25 pb-12 pb-lg-20" />
         <Testimonials className="pt-12 pt-lg-19 pb-12 pb-lg-17" />
         <FAQ />
-        <Company className="bg-default-1 pt-13 pt-md-17 pt-lg-24 pb-13 pb-md-14 pb-lg-23" />
-        <Categories className="bg-default-1 pt-lg-13 pb-md-7 pb-lg-11 position-relative" />
-        <Jobs className="pt-14 pt-md-18 pt-lg-30 bg-default-2 pb-9 pb-md-13 pb-lg-29" />
-        <Content1 className="pt-21 pt-md-24 pt-lg-32 pb-15 pb-md-19 pb-lg-30" />
-        <Blog className="bg-default-1 pt-14 pt-md-18 pt-lg-27 pb-13 pb-md-17 pb-lg-26" />
       </PageWrapper>
     </>
   );
