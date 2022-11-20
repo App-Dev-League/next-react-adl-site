134e0b62d6913f5c02069fd2efd158acf6732c54
Krish Maniar <56781484+krinetic1234@users.noreply.github.com>
2022-11-20 15:13:19 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -66,7 +66,7 @@ const IndexPage = () => {
                 <iframe
                   width="480"
                   height="270"
-                  src="https://www.youtube.com/embed/AUvVho1o7iY"
+                  src="https://www.youtube.com/embed/2qYflteLQaI"
                   title="YouTube video player"
                   frameborder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
